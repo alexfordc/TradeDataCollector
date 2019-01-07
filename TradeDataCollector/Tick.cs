78d29b8ebc39d937391a5b406251f337a45f35b3
chs <tianzhong_work@msn.com>
2019-01-07 12:19:10 +0800

    add data class

--- TradeDataCollector/Tick.cs
+++ TradeDataCollector/Tick.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TradeDataCollector
 {
-    public class Class1
+    class Tick
     {
     }
 }
---
+++ TradeDataCollector/Trade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeDataCollector
+{
+    public class Trade
+    {
+        public DateTime DateTime;
+        public float Price;
+        public double Size;
+        public double Amount;
+        public char BuyOrSell;
+    }
+}
